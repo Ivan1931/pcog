2a12be80e009fa80fbdbae6c14b0f218a888bc0d
jonah-hooper <jonah.graham.hooper@gmail.com>
2017-07-11 23:12:28 +0200

    Initial commit

---
+++ pcog/pcoq.py
@@ -0,0 +1,4 @@
+from pcog.tiger_door import tiger_door_simulation
+
+if __name__ == "__main__":
+    tiger_door_simulation()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pcog',
+    version='0.1dev',
+    packages=['pcog',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
